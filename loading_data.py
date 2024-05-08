bb98420f4fc60bcbc0b45eb05de32a7eaa0d50d1
sara <sara.caramaschi@mau.se>
2024-05-08 17:47:53 +0200

    Initial commit - loading functions

--- loading_data.py
+++ loading_data.py
@@ -3,7 +3,7 @@ from settings import running_settings, loaders_functions
 
 if __name__ == '__main__':
     main_path = running_settings.main_path
-    project_path = main_path + os.sep + 'walking_dataset'
+    project_path = main_path + os.sep + 'walking_dataset_for_6MWT'
     # Loading data
     selection, info_all_tests = loaders_functions.loader(project_path + os.sep + 'data',
                                                          blacklist=running_settings.run_settings['blacklist'])
